f4d711e002dfa1260f2cfdfe2c875631ba8ddd84
Ayush Tripathi <ayushtripathi1012@gmail.com>
2023-08-19 21:04:08 +0530

    first commit

---
+++ ps6.js
@@ -0,0 +1,18 @@
+const prompt= require("prompt-sync")();
+//PROBLEM 1
+// let age =prompt("Enter the age")
+// age=Number.parseInt(age)
+// const candrive= (age)=>{
+//     return age>=18?true:false
+// }
+// if (candrive(age)){
+//     alert("YES")
+
+// }
+// else{
+//     alert("NO")
+// }
+
+//PROBLEM 2
+
+ 
